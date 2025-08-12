e1074170721f002630b29a0429a62204529c7bbb
vinicius-leonel <viniciuslps.cms@gmail.com>
2025-08-12 15:31:26 -0300

    Iniciando projeto 'backend-challenge'

---
+++ src/main/java/br/dev/viniciusleonel/backend_challenge/BackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.dev.viniciusleonel.backend_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/viniciusleonel/backend_challenge/BackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.viniciusleonel.backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
