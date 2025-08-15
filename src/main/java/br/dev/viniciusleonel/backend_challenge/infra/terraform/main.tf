f952ca0c86db0a025511335c82ea3e15ae3ee11c
vinicius-leonel <viniciuslps.cms@gmail.com>
2025-08-15 09:59:39 -0300

    fix: liberando porta 8080 na aws

--- src/main/java/br/dev/viniciusleonel/backend_challenge/infra/terraform/main.tf
+++ src/main/java/br/dev/viniciusleonel/backend_challenge/infra/terraform/main.tf
@@ -31,6 +31,13 @@ resource "aws_security_group" "backend-challenge-group" {
   }
 
   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
